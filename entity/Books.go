f3f9c05ee64e5fe712a99dafa18dde5206131404
songdongli_yxl <songdongli@xinli001.com>
2021-05-14 13:20:46 +0800

    json格式返回

--- entity/Books.go
+++ entity/Books.go
@@ -1,14 +1,14 @@
 package entity
 
-type Books struct{
-	BookID int `gorm:"column:book_id;AUTO_INCREMENT;PRIMARY_KEY"`
-	BookName string `gorm:"column:book_name;type:varchar(50)"`
-	BookIntr string `gorm:"column:book_intr;type:text"`
-	BookPrice1 float64 `gorm:"column:book_price1;type:decimal"`
-	BookPrice2 float64 `gorm:"column:book_price2;type:decimal"`
-	BookAuthor string `gorm:"column:book_author;type:varchar(50)"`
-	BookPress string `gorm:"column:book_press;type:varchar(50)"`
-	BookDate string `gorm:"column:book_date;type:varchar(50)"`
-	BookKind int `gorm:"column:book_kind;type:int"`
+type Books struct {
+	BookID     int     `json:"book_id" gorm:"column:book_id;AUTO_INCREMENT;PRIMARY_KEY"`
+	BookName   string  `json:"book_name" gorm:"column:book_name;type:varchar(50)"`
+	BookIntr   string  `json:"book_intr" gorm:"column:book_intr;type:text"`
+	BookPrice1 float64 `json:"book_price1" gorm:"column:book_price1;type:decimal"`
+	BookPrice2 float64 `json:"book_price2" gorm:"column:book_price2;type:decimal"`
+	BookAuthor string  `json:"book_author" gorm:"column:book_author;type:varchar(50)"`
+	BookPress  string  `json:"book_press" gorm:"column:book_press;type:varchar(50)"`
+	BookDate   string  `json:"book_date" gorm:"column:book_date;type:varchar(50)"`
+	BookKind   int     `json:"book_kind" gorm:"column:book_kind;type:int"`
 }
 type BookList []*Books
